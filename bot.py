63275b434eb4659d563c7e205ab1e114994f24c4
rudes <jacobriddle94@gmail.com>
2021-06-04 10:44:38 -0400

    try setting different logger levels

--- bot.py
+++ bot.py
@@ -11,13 +11,16 @@ intents.members = True
 intents.presences = True
 client = discord.Client(intents=intents)
 
-logging.basicConfig(format="%(asctime)s %(name)s:%(levelname)-8s %(message)s",
-        filename="/var/log/snakebot.log", level=logging.INFO)
+logging.basicConfig(format="%(asctime)s %(name)s:%(levelname)-8s %(message)s", filename="/var/log/snakebot.log")
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+discord_log = logging.getLogger(discord)
+discord_log.setLevel(logging.INFO)
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game(name="snakED"))
-    logging.info('on_ready,{0.user},presence state set'.format(client))
+    log.info('on_ready,{0.user},presence state set'.format(client))
 
 @client.event
 async def on_member_update(before,after):
@@ -55,7 +58,7 @@ async def sync_handler(m):
         userCount += 1
         await live_handler(user)
     await m.delete()
-    logging.info("sync_handler,synced {} users".format(userCount))
+    log.info("sync_handler,synced {} users".format(userCount))
 
 async def live_handler(after):
     live_role = after.guild.get_role(399778773265940481)
@@ -72,7 +75,7 @@ async def live_handler(after):
     if not  live_streaming:
         if live_role_exists:
             await after.remove_roles(live_role, )
-            logging.info("live_handler,removing role from {}, not streaming"
+            log.info("live_handler,removing role from {}, not streaming"
                     .format(after.name))
         return
     sub_role = False
@@ -85,13 +88,13 @@ async def live_handler(after):
         return
     if  live_streaming:
         await after.add_roles(live_role, )
-        logging.info("live_handler,adding role to {}".format(after.name))
+        log.info("live_handler,adding role to {}".format(after.name))
         return
 
 async def sub_handler(m):
     if not lock_handler():
         return
-    logging.info("{} requested sub role".format(m.author.name))
+    log.info("{} requested sub role".format(m.author.name))
     await m.channel.send(m.author.mention+" to receieve the Subscribers Role please link your twitch account to discord (Settings > Connections > Twitch Icon) and wait 30 minutes to an hour.")
 
 async def f_handler(m):
@@ -118,11 +121,11 @@ async def esports_background_task():
         if r.json()["data"]:
             await esportsChannel.edit(name="esports",
                     topic="https://twitch.tv/Rainbow6")
-            logging.info("Changing channel to #esports")
+            log.info("Changing channel to #esports")
         else:
             await esportsChannel.edit(name="meta-discussion",
                     topic="Discussion around the meta of the game")
-            logging.info("Changing channel to #meta-discussion")
+            log.info("Changing channel to #meta-discussion")
         await asyncio.sleep(300)
 
 def lock_handler():
